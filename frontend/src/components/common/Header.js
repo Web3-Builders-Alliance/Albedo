671054a4e71c4ba6d3578a8632fb20d73b0cb820
Kellen James <Kellenkjames@gmail.com>
2023-09-12 15:28:31 -0400

    Added hover effect to nav links

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -11,13 +11,13 @@ const Header = () => {
     <nav className='nav'>
       <ul className='nav-list'>
         <li className='nav-item'>
-          <a href='#how-it-works'>How it Works</a>
+          <a className='link' href='#how-it-works'>How it Works</a>
         </li>
         <li className='nav-item'>
-          <a href='#features'>Features</a>
+          <a className='link' href='#features'>Features</a>
         </li>
         <li className='nav-item'>
-          <a className='btn-primary' href='#login'>Login/Sign-Up</a>
+          <a className='btn-secondary login-signup' href='#login'>Login/Sign-Up</a>
         </li>
       </ul>
     </nav>
