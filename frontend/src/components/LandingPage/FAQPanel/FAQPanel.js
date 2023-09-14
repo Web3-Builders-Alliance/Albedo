26da43774949c5189b98140049b4a7f75549f42f
Kellen James <Kellenkjames@gmail.com>
2023-09-14 15:03:40 -0400

    Integrate SASS and Bootstrap for FAQPanel component

--- frontend/src/components/LandingPage/FAQPanel/FAQPanel.js
+++ frontend/src/components/LandingPage/FAQPanel/FAQPanel.js
@@ -1,9 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './FAQPanel.scss';
 
 export default function BasicAccordion() {
@@ -28,54 +23,78 @@ export default function BasicAccordion() {
   }, []);
 
   return (
-    <div ref={panelRef} className='faq-section hidden'>
-      <h2>FAQ</h2>
-      <Accordion className='accordion'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className='title'>What is Albedo?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          Albedo is a decentralized marketplace that leverages IoT sensors and blockchain technology to facilitate seamless interactions between data providers and consumers. Our platform aims to revolutionize how sensor data is verified, shared, and utilized, making it easier for you to bring or consume real-world data in a secure and transparent manner.
-
-          Example: Imagine a smart city where parking spots are equipped with IoT sensors. These sensors provide real-time data on parking availability. As a Data Provider, you can monetize this data by listing it on Albedo. On the flip side, as a Data Consumer, you can access this data to find parking spots easily and pay for them securely through smart contracts.
-
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion className='accordion'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className='title'>How Does Albedo Ensure Data Reliability?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          Reliability is at the core of what we do. All sensor data that comes into Albedo is first verified by trusted data oracles. Once verified, the data is stored on the blockchain, ensuring it is immutable and trustworthy. This rigorous verification process allows us to provide high-quality, reliable data to consumers.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion className='accordion'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography className='title'>How Can I Get Started as a Data Provider or Consumer?</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-          Getting started is easy. Simply sign up on our platform and choose your role as either a Data Provider or Consumer. If you're a Data Provider, you can begin uploading data from your IoT sensors following our easy-to-use guidelines. As a Data Consumer, you can browse the marketplace to find the data that suits your needs and access it in real-time.
-
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+    <div ref={panelRef} className="accordion hidden" id="accordionExample">
+      <h2 id='#how-it-works'>FAQ</h2>
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            What Is Albedo?
+          </button>
+        </h2>
+        <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+          <div className="accordion-body">
+            <p>
+              Albedo is a decentralized marketplace that leverages IoT sensors and blockchain technology
+              to facilitate seamless interactions between data providers and consumers. Our platform aims
+              to revolutionize how sensor data is verified, shared, and utilized, making it easier for you
+              to bring or consume real-world data in a secure and transparent manner.
+              Example: Imagine a smart city where parking spots are equipped with IoT sensors. These sensors
+              provide real-time data on parking availability. As a Data Provider, you can monetize this data
+              by listing it on Albedo. On the flip side, as a Data Consumer, you can access this data to find
+              parking spots easily and pay for them securely through smart contracts.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTwo"
+            aria-expanded="true"
+            aria-controls="collapseTwo"
+          >
+            How Does Albedo Ensure Data Reliability?
+          </button>
+        </h2>
+        <div id="collapseTwo" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+          <div className="accordion-body">
+            <p>
+            Reliability is at the core of what we do. All sensor data that comes into Albedo is first verified by trusted data oracles. Once verified, the data is stored on the blockchain, ensuring it is immutable and trustworthy. This rigorous verification process allows us to provide high-quality, reliable data to consumers.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseThree"
+            aria-expanded="true"
+            aria-controls="collapseThree"
+          >
+            How Can I Get Started as a Data Provider or Consumer?
+          </button>
+        </h2>
+        <div id="collapseThree" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+          <div className="accordion-body">
+            <p>
+            Getting started is easy. Simply sign up on our platform and choose your role as either a Data Provider or Consumer. If you're a Data Provider, you can begin uploading data from your IoT sensors following our easy-to-use guidelines. As a Data Consumer, you can browse the marketplace to find the data that suits your needs and access it in real-time.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
