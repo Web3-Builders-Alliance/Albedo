d84dffc0e86e70040004325b6e67ed53fadc2533
Kellen James <Kellenkjames@gmail.com>
2023-09-14 14:01:25 -0400

    Upgrade to SASS and Bootstrap; refactor Header component

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -3,25 +3,27 @@ import './Header.scss';
 
 const Header = () => {
   return (
-    <header className="header">
-    <div className="logo">
-      {/* Placeholder for Logo */}
-      Albedo
-    </div>
-    <nav className='nav'>
-      <ul className='nav-list'>
-        <li className='nav-item'>
-          <a className='link' href='#how-it-works'>How it Works</a>
-        </li>
-        <li className='nav-item'>
-          <a className='link' href='#features'>Features</a>
-        </li>
-        <li className='nav-item'>
-          <a className='btn-secondary login-signup' href='#login'>Login/Sign-Up</a>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Albedo</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#how-it-works">How it Works</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#features">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link btn-secondary login-signup" href="#login">Login/Sign-Up</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    </header>
   );
 };
 
