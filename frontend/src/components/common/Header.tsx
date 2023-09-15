69a57cd06de82f3f1a9eeecebd818edc21b34e44
Kellen James <Kellenkjames@gmail.com>
2023-09-15 17:56:48 -0400

    Navbar update, replace Albedo with Built on Solana

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -5,7 +5,7 @@ export const Header: FC = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand fw-bold" href="/">Albedo</a>
+        <a className="navbar-brand fw-bold" href="/">Built on Solana</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
