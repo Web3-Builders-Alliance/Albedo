dc4ba474c7247769102c52a4bb7bb620a4c88f46
Kellen James <Kellenkjames@gmail.com>
2023-09-18 13:57:17 -0400

    Add Bootstrap-based form validation to SignupForm component

--- frontend/src/components/SignupPage/SignupForm/index.tsx
+++ frontend/src/components/SignupPage/SignupForm/index.tsx
@@ -3,15 +3,32 @@ import './SignupForm.scss';
 
 export const SignupForm: FC = () => {
   return (
-    <form className="signup-form">
+    <form className="signup-form needs-validation" noValidate>
+      {/* Email Field */}
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" required />
+        <div className="invalid-feedback">
+          Please provide a valid email.
+        </div>
+        <div className="valid-feedback">
+          Looks good!
+        </div>
       </div>
+
+      {/* Password Field */}
       <div className="mb-3">
         <label htmlFor="password" className="form-label">Password</label>
-        <input type="password" className="form-control" id="password" />
+        <input type="password" className="form-control" id="password" required />
+        <div className="invalid-feedback">
+          Please provide a password.
+        </div>
+        <div className="valid-feedback">
+          Looks good!
+        </div>
       </div>
+
+      {/* Submit Button */}
       <button type="submit" className="btn btn-primary w-100">Submit</button>
     </form>
   );
