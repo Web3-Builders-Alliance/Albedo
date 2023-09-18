40984bed7ed80604c637678196d0af4b481a5eac
Kellen James <Kellenkjames@gmail.com>
2023-09-18 17:03:53 -0400

    Implement frontend form validation and submission for Signup Page

--- frontend/src/components/SignupPage/SignupForm/index.tsx
+++ frontend/src/components/SignupPage/SignupForm/index.tsx
@@ -1,35 +1,62 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './SignupForm.scss';
 
 export const SignupForm: FC = () => {
-  return (
-    <form className="signup-form needs-validation" noValidate>
-      {/* Email Field */}
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" required />
-        <div className="invalid-feedback">
-          Please provide a valid email.
-        </div>
-        <div className="valid-feedback">
-          Looks good!
-        </div>
-      </div>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    fetch('http://localhost:3001/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log('Error:', error));
+};
 
-      {/* Password Field */}
-      <div className="mb-3">
-        <label htmlFor="password" className="form-label">Password</label>
-        <input type="password" className="form-control" id="password" required />
-        <div className="invalid-feedback">
-          Please provide a password.
-        </div>
-        <div className="valid-feedback">
-          Looks good!
-        </div>
-      </div>
-
-      {/* Submit Button */}
-      <button type="submit" className="btn btn-primary w-100">Submit</button>
-    </form>
+return (
+  <form className="signup-form needs-validation" noValidate onSubmit={handleSubmit}>
+  {/* Email Field */}
+  <div className="mb-3">
+  <label htmlFor="email" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="email" aria-describedby="emailHelp" 
+  value={email} 
+  onChange={e => setEmail(e.target.value)} 
+  required />
+  <div className="invalid-feedback">
+  Please provide a valid email.
+  </div>
+  <div className="valid-feedback">
+  Looks good!
+  </div>
+  </div>
+  
+  {/* Password Field */}
+  <div className="mb-3">
+  <label htmlFor="password" className="form-label">Password</label>
+  <input type="password" className="form-control" id="password" 
+  value={password} 
+  onChange={e => setPassword(e.target.value)} 
+  required />
+  <div className="invalid-feedback">
+  Please provide a password.
+  </div>
+  <div className="valid-feedback">
+  Looks good!
+  </div>
+  </div>
+  
+  {/* Submit Button */}
+  <button type="submit" className="btn btn-primary w-100">Submit</button>
+  </form>
   );
 }
