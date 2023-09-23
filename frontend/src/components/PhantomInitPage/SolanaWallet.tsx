572e59e6d2843d8b1275cf61fbe10c579615544c
Kellen James <Kellenkjames@gmail.com>
2023-09-23 17:33:14 -0400

    Commit Message: --------------------- feat(auth): Minimize signInData and fix domain issue in Solana sign-in
    - Removed hardcoded optional fields from SolanaSignInInput object to simplify and secure the authentication flow.
    - Fixed the domain mismatch issue by aligning the environment variable with the actual running domain.
    - Replaced hardcoded  with a variable set according to the connected network.
    
    This commit aims to resolve bugs in the Solana wallet sign-in process, making it more robust and maintainable.
    ---------------------

--- frontend/src/components/PhantomInitPage/SolanaWallet.tsx
+++ frontend/src/components/PhantomInitPage/SolanaWallet.tsx
@@ -4,12 +4,9 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
   WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import { SolanaSignInInput } from "@solana/wallet-standard-features";
-import { SignupForm } from '../SignupPage/SignupForm';
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
@@ -22,7 +19,7 @@ export const SolanaWallet: FC<PropsWithChildren> = ( {children} ) => {
   }, []);
   
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Testnet;
+  const network = WalletAdapterNetwork.Devnet;
   
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
--- frontend/src/components/SignupPage/SignupForm/index.tsx
+++ frontend/src/components/SignupPage/SignupForm/index.tsx
@@ -3,35 +3,46 @@ import { useWallet } from '@solana/wallet-adapter-react';
 import { SystemProgram, Transaction, Connection } from '@solana/web3.js';
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
 import { WalletMultiButton, WalletDisconnectButton } from '@solana/wallet-adapter-react-ui';
+import { SolanaSignInInput, SolanaSignInOutput } from '@solana/wallet-standard-features';
 
 export const SignupForm: FC = () => {
   const { publicKey, sendTransaction, connected, signIn } = useWallet();
   const [message, setMessage] = useState('');
-
+  
   const connection = useMemo(() => {
     return new Connection('https://api.testnet.solana.com/');
   }, []);
-
-  console.log("Frontend Network: ", connection.rpcEndpoint);
   
   const signInSolana = useCallback(async () => {
-    console.log("Is Wallet Connected: ", connected);
-    console.log("Public Key: ", publicKey?.toBase58());
-    console.log("Is sendTransaction Function Available: ", typeof sendTransaction === 'function');
-
+    console.log("About to call signIn()");
+    console.log("Is connected?", connected);
+    console.log("Type of signIn", typeof signIn);
+    
     try {
       if (!connected && typeof signIn === 'function') {
-        await signIn(); // Manually initiate connection
+        try {
+          await signIn();
+          console.log("signIn() called successfully");
+        } catch (error: any) {
+          console.log("signIn() failed:", error);
+        }
+        console.log("signIn() called successfully");
       }
-  
+
       if (!publicKey || typeof sendTransaction !== 'function' || !connection) {
         throw new WalletNotConnectedError();
       }
-      
-      // Fetch the signInInput from your backend
+
+      console.log("About to fetch signInInput");
+
       const res = await fetch(process.env.REACT_APP_SIGNIN_ENDPOINT || 'http://localhost:3001/api/signInInput');
+      console.log("Fetched signInInput: ", res);
+
       if (!res.ok) throw new Error('Failed to fetch sign-in input');
-      const signInInput = await res.json();
+      
+      const signInInput: SolanaSignInInput = await res.json(); // Type-checking here
+
+      console.log("Parsed signInInput: ", signInInput);
       
       // Generate a dummy transaction just to get a signature
       const instruction = SystemProgram.transfer({
@@ -39,12 +50,9 @@ export const SignupForm: FC = () => {
         toPubkey: publicKey,
         lamports: 0, // 0 lamports transfer means no "actual" transfer
       })
-  
+      
       // Create a transaction containing this instruction
       const transaction = new Transaction().add(instruction);
-
-      console.log("Instruction Object: ", instruction);
-      console.log("Transaction Object: ", transaction);
       
       // Sign and send the transaction, receiving the signature as the confirmation
       let signature: string | null = null;
@@ -52,62 +60,81 @@ export const SignupForm: FC = () => {
       try {
         signature = await sendTransaction(transaction, connection);
         console.log("Transaction Signature: ", signature);
-        } catch (err: any) {
-            console.log("Error Sending Transaction: ", err);
-            throw err;
-        }
-
+      } catch (err: any) {
+        console.log("Error Sending Transaction: ", err);
+        throw err;
+      }
+      
+      // Check if `signInInput` and `signature` have values before the fetch call.
+      console.log("Debug: signInInput:", signInInput, "Signature:", signature);
+      
+      const outputData: SolanaSignInOutput = {
+        account: {
+          publicKey: new Uint8Array(/* ... */),
+          address: "",
+          chains: [],
+          features: []
+        },
+        signature: new Uint8Array(/* ... */),
+        signedMessage: new Uint8Array(/* ... */)
+      };
+      
+      //! Explicit logging to debug
+      console.log("Preparing to send to backend: ", {
+        input: signInInput, 
+        signature: outputData
+      });
+      
       // Use the signature to authenticate the user on your backend
       const verifyRes = await fetch('http://localhost:3001/api/verifyOutput', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ input: signInInput, signature }),
-      });
-  
-      const { success } = await verifyRes.json();
-
-      console.log('Verification response:', { success }); // <-- Add this log
-  
-      if (success) {
-        setMessage('Successfully signed in with Solana');
-      } else {
-        setMessage('Failed to verify Solana sign-in');
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ signInInput, outputData }),
+    });
+    
+    
+    const { success } = await verifyRes.json();
+    
+    console.log('Verification response:', { success }); // <-- Add this log
+    
+    if (success) {
+      setMessage('Successfully signed in with Solana');
+    } else {
+      setMessage('Failed to verify Solana sign-in');
     }
     
-    } catch (err: any) {
-      console.error("Detailed Error:", { message: err.message, stack: err.stack }); // <-- Add this log for more detail
-      setMessage(`Error: ${err.message}`);
-    }
-  }, [publicKey, sendTransaction, connection, signIn, connected]);
+  } catch (err: any) {
+    console.error("Detailed Error:", { message: err.message, stack: err.stack }); // <-- Add this log for more detail
+    setMessage(`Error: ${err.message}`);
+  }
+}, [publicKey, sendTransaction, connection, signIn, connected]);
 
-  return (
-    <div className="signup-form text-center">
-      {/* Show Message */}
-      {message && <div className="alert alert-info">{message}</div>}
-
-      {connected ? (
-        <>
-          <div className="mb-3">
-            Connected with Public Key: {publicKey?.toBase58() || ''}
-          </div>
-          <WalletDisconnectButton className="btn btn-primary w-100" />
-        </>
-      ) : (
-        <>
-          <WalletMultiButton className="btn btn-primary w-100" />
-        </>
+return (
+  <div className="signup-form text-center">
+  {/* Show Message */}
+  {message && <div className="alert alert-info">{message}</div>}
+  
+  {connected ? (
+    <>
+    <div className="mb-3">
+    Connected with Public Key: {publicKey?.toBase58() || ''}
+    </div>
+    <WalletDisconnectButton className="btn btn-primary w-100" />
+    </>
+    ) : (
+      <>
+      <WalletMultiButton className="btn btn-primary w-100" />
+      </>
       )}
-
+      
       {/* Optional: Add a button to trigger Solana-based sign-up */}
       <button 
-          type="button"  
-          disabled={!connected} 
-          className="btn btn-secondary w-100 mt-3" 
-          onClick={signInSolana}>
-          Sign Up with Solana
+      type="button"  
+      disabled={!connected} 
+      className="btn btn-secondary w-100 mt-3" 
+      onClick={signInSolana}>
+      Sign Up with Solana
       </button>
-    </div>
-  );
-};
+      </div>
+      );
+    };
