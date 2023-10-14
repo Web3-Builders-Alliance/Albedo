99b3edc0757257bfbf417ac9885bf464b5b2463f
Kellen James <Kellenkjames@gmail.com>
2023-10-14 18:59:13 -0400

    feat(cors): Update CORS middleware for production domain compatibility

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,27 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-// import { signupRoute } from './routes/signup';
-// import { verifyEmailRoute } from './routes/emailVerification';
 import { createSignInData } from './routes/siws/signInInput';
-
 import siwsRoutes from '../src/routes/siws/index';
 
 // Initialize express
 const app = express();
-const port = 3001; // You can choose another port if you like
+const port = 3001;
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://albedo.digital/'];
+// List of allowed origins; added 'https://albedo.digital/' as an example
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://albedo.digital'];
 
-// Enable CORS from frontend
+// Updated CORS middleware
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      console.error(`Origin ${origin} not allowed by CORS`);
+      callback(null, false);
     }
   },
+  credentials: true,  // Added this line to handle credentials
 }));
 
 // Middleware for parsing JSON
@@ -33,12 +33,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-//! Archive: Register the signup route
-// app.post('/signup', signupRoute);
-
-//! Archve: Add verifyEmailRoute
-// app.post('/api/verify-token', verifyEmailRoute);
-
 // Sign in With Solana routes
 app.use('/api', siwsRoutes);
 
@@ -46,8 +40,9 @@ app.use('/api', siwsRoutes);
 app.get('/api/getSignInData', async (req, res) => {
   const signInData = await createSignInData();
   res.json(signInData);
-})
+});
 
+// Listening on the designated port
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
